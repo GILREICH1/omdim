f558e06ee96527400b223e24e89cd27aa89e4e75
Gil Reich <gil.reich@acronis.com>
2024-02-16 16:50:20 +0200

    updated tsl

--- translationService.ts/translationText.ts
+++ translationService.ts/translationText.ts
@@ -18,37 +18,37 @@ enum TslKeys {
 type translationMap = Record<TslKeys, string>
 
 const enText: translationMap = {
-    intro: "Hi! 😊 send /new to get started adding a new event!\nSend /help for more options",
-    changeLang: 'send /en to chat in English',
+    intro: "Hi! 😊 Send /new to get started adding a new event!\nSend /help for more options",
+    changeLang: 'Send /en to chat in English',
     cancel: 'Cancel',
-    switched: 'switched',
+    switched: 'Switched',
     continue: 'Send /restart to start again',
-    newEvent: 'great let\'s get going! What the name of the event?',
-    eventSaved: 'event saved',
+    newEvent: 'Great let\'s get going! What the name of the event?',
+    eventSaved: 'Event saved',
     infoMissing: 'info missing',
     saveDate: 'Great! Now please send the date in the format mm-dd, for example 02-14 for Feb 14th',
-    saveParticipants: 'great! Now please send the list of participants seperated by commas',
-    confirmName: 'name:',
-    confirmDate: 'date: ',
+    saveParticipants: 'Great! Now please send the list of participants seperated by commas',
+    confirmName: 'Name:',
+    confirmDate: 'Date: ',
     confirmParticipants: 'Participants',
-    confirm: 'send /confirm to save'
+    confirm: 'Send /confirm to save'
 }
 
 const heText: translationMap = {
-    intro: "\"הי! 😊 שלח \"/new\" כדי להתחיל בהוספת אירוע חדש!\ n\ שלח \"/help\" לאפשרויות נוספות\",",
-    changeLang: "לצ'אט בעברית, שלח /he",
+    intro: "\"הי! 😊 שלח.י \"/new\" כדי להתחיל בהוספת אירוע חדש!\ n\ שלח.י \"/help\" לאפשרויות נוספות\",",
+    changeLang: "לצ'אט בעברית, שלח.י /he",
     cancel: 'בטל',
     switched: 'החלפנו',
-    continue: 'שלח/restart מחדש כדי להתחיל מחדש',
+    continue: 'שלח.י/restart מחדש כדי להתחיל מחדש',
     newEvent: "מעולה בואו נצא לדרך! מה שם האירוע?",
     eventSaved: 'האירוע נשמר',
     infoMissing: 'חסר מידע',
     saveDate: 'מושלם, נא לשלוח את תאריך האירוע בפורמט mm-dd, לדוגמה: 02-14 ל-14 בפברואר',
-    saveParticipants: 'גדול! כעת נא לשלוח את רשימת המשתתפים מופרדים בפסיקים',
+    saveParticipants: 'גדול! נא לשלוח את רשימת המשתתפים.ות מופרדים.ות בפסיקים',
     confirmName: ':שם',
     confirmDate: 'תאריך: ',
-    confirmParticipants: 'משתתפים',
-    confirm: 'שלח /confirm כדי לשמור'
+    confirmParticipants: 'משתתפים.ות',
+    confirm: 'שלח.י /confirm כדי לשמור'
 }
 
 
